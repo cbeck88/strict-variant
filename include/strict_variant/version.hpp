12b6ac641745b3aa065cffdedf5a4777d14be06d
Chris Beck <beck.ct@gmail.com>
2016-12-09 00:34:39 -0500

    add version.hpp file

---
+++ include/strict_variant/version.hpp
@@ -0,0 +1,10 @@
+//  (C) Copyright 2016 Christopher Beck
+
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#define STRICT_VARIANT_VERSION_MAJOR 0
+#define STRICT_VARIANT_VERSION_MINOR 3
+#define STRICT_VARIANT_VERSION_PATCH 1
