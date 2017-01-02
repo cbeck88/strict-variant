dc5668bb3bb95a4bb170babb15e163736ddbbe6f
Chris Beck <beck.ct@gmail.com>
2017-01-01 19:52:31 -0500

    new years copyright update

--- find_and_replace.sh
+++ find_and_replace.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find include/strict_variant test/include test/ -type f -name "*.?pp" -print0 | xargs -0 sed -i "s|SAFE_VARIANT_|STRICT_VARIANT_|g"
+find include bench example test -type f -name "*.?pp" -print0 | xargs -0 sed -i "s|//  (C) Copyright 2016 Christopher Beck|//  (C) Copyright 2016 - 2017 Christopher Beck|g"
