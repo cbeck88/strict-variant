0097ac300de4a936807c1312c55033f08cca54f5
Chris Beck <beck.ct@gmail.com>
2018-01-28 16:30:47 -0800

    copyright update

--- find_and_replace.sh
+++ find_and_replace.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find include bench example test -type f -name "*.?pp" -print0 | xargs -0 sed -i "s|//  (C) Copyright 2016 Christopher Beck|//  (C) Copyright 2016 - 2017 Christopher Beck|g"
+find include bench example test -type f -name "*.?pp" -print0 | xargs -0 sed -i "s|//  (C) Copyright 2016 - 2017 Christopher Beck|//  (C) Copyright 2016 - 2018 Christopher Beck|g"
